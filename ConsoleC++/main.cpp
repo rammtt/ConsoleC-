6cfcfc51d82c642bcdc06c89d0dade9c24751f38
Admin <Admin@DESKTOP-ERAQKNL>
2025-05-28 00:10:27 +0300

    Добавьте файлы проекта.

---
+++ ConsoleC++/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	float num = 5.321;
+	std::cout << num << std::endl;
+
+	char sym = '%';
+	std::cout << sym << std::endl;
+
+	bool is_has_car = false;
+	std::cout << is_has_car << std::endl;
+
+	return 0;
+
+
+
+}
