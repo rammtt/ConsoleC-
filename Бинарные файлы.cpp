672894a71449b167b6874694328bfadf9a824aa9
rammtt <aupov2@mail.ru>
2025-05-29 18:55:41 +0300

    Add files via upload

---
+++ Бинарные файлы.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+int main() {
+
+	//int x = 54434325;
+	//ofstream fout("txt.bin",ios_base::binary);
+	//fout.write((char*)&x, sizeof(int));
+	//fout.close();
+	int tmp;
+	ifstream fout("txt.bin", ios_base::binary);
+	fout.read((char*)&tmp, sizeof(int));
+	fout.close();
+	cout << tmp << endl;
+	
+
+	return 0;
+}
